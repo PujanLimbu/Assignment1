5b35edf38d6325368336ee149c26f20866f7136c
PujanLimbu <pujantumkhewa@gmail.com>
2025-06-14 22:08:02 +0000

    data added in MySQL

--- mysql.sql
+++ mysql.sql
@@ -13,5 +13,6 @@ INSERT INTO employee VALUES ('2','Jack','Williams','Empathy','alien');
 INSERT INTO employee VALUES ('3','Elon','Musk','Engineer','USA');
 INSERT INTO employee VALUES ('4','Kp','oli','Engineer','canada');
 INSERT INTO employee VALUES ('5','Kps','oli','Engineer','canada');
+INSERT INTO employee VALUES ('6','hero','rai','doctor','canada');
 SELECT * FROM employee;
 
