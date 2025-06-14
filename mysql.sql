4f5ce9a20a671cd7e40078f151ac4a613a3325d9
EC2 Default User <ec2-user@ip-172-31-89-60.ec2.internal>
2025-06-14 18:45:17 +0000

    Files Added

---
+++ mysql.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE IF NOT EXISTS employees;
+USE employees;
+
+CREATE TABLE IF NOT EXISTS employee(
+emp_id VARCHAR(20),
+first_name VARCHAR(20),
+last_name VARCHAR(20),
+primary_skill VARCHAR(20),
+location VARCHAR(20));
+
+INSERT INTO employee VALUES ('1','Pujan','Limbu','Student','scarborough');
+INSERT INTO employee VALUES ('2','Jack','Williams','Empathy','alien');
+INSERT INTO employee VALUES ('3','Elon','Musk','Engineer','USA');
+INSERT INTO employee VALUES ('4','Kp','oli','Engineer','canada');
+SELECT * FROM employee;
+
